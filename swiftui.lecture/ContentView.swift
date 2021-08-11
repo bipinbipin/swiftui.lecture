20f003aead662c9b628f36c801d4f54d589d0101
Bipin <developer.support@astontech.com>
2021-08-11 17:30:06 -0500

    initial commit

--- swiftui.lecture/ContentView.swift
+++ swiftui.lecture/ContentView.swift
@@ -7,11 +7,89 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+final class ContentViewModel: ObservableObject {
+    
+    @Published var firstName: String = ""
+    @Published var lastName: String = ""
+    @Published var serverTime: String = ""
+    
+    func getData() {
+        NetworkManager.shared.getData { [self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let apiResponse):
+                    self.firstName = apiResponse.firstName
+                    self.lastName = apiResponse.lastName
+                    self.serverTime = apiResponse.timeStamp
+                    print("success - \(apiResponse)")
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        }
     }
+}
+
+struct ContentView: View {
+    
+    @StateObject var viewModel = ContentViewModel()
+    
+    var body: some View {
+        ZStack {
+            Color("brandPrimary")
+                .ignoresSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                
+                Image("aston-logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300, height: 300)
+                
+                Text("Welcome")
+                    .bold()
+                    .font(.system(size: 24))
+                    .foregroundColor(.white)
+                
+                Text("Guests")
+                    .padding(.bottom, 20)
+                
+                Button {
+                    viewModel.getData()
+                    print("button pressed")
+                } label: {
+                    BrandButton(title: "Answer a question", color: "brandSecondary")
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Text("\(viewModel.firstName)")
+                    Text("\(viewModel.lastName)")
+                }
+                
+                Text("\(viewModel.serverTime)")
+                
+            }
+        }
+    }
+}
+
+struct BrandButton: View {
+    var title: String
+    var color: String
+    
+    var body: some View {
+        Text(title)
+            .font(.title3)
+            .fontWeight(.semibold)
+            .frame(width: 240, height: 50)
+            .background(Color(color))
+            .foregroundColor(.white)
+            .cornerRadius(8)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
--- swiftui.lecture/NetworkManager.swift
+++ swiftui.lecture/NetworkManager.swift
@@ -7,14 +7,40 @@
 
 import SwiftUI
 
-struct NetworkManager: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+struct ApiResponse: Codable {
+    var firstName: String
+    var lastName: String
+    var timeStamp: String
 }
 
-struct NetworkManager_Previews: PreviewProvider {
-    static var previews: some View {
-        NetworkManager()
+final class NetworkManager: ObservableObject {
+    
+    // create a single static instance of this class
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    let url = "https://demo.astontech.com/get-data"
+    
+    func getData(completed: @escaping (Result<ApiResponse, Error>) -> Void) {
+        let task = URLSession.shared.dataTask(with: URLRequest(url: URL(string: url)!)) { data, response, error in
+            // check that we get a 200 response
+            
+            // check that data is valid
+            guard let data = data else {
+                print("invalid data")
+                return
+            }
+            
+            do {
+                print("\(data)")
+                let decodedResponse = try JSONDecoder().decode(ApiResponse.self, from: data)
+                completed(.success(decodedResponse))
+            } catch {
+                print("error decoding json")
+            }
+        }
+        task.resume()
     }
+    
 }
