84e28acd3de8a519f2aca0360049c66ad402cbe9
Bipin <developer.support@astontech.com>
2021-08-11 17:29:01 -0500

    first commit

---
+++ swiftui.lecture/NetworkManager.swift
@@ -0,0 +1,20 @@
+//
+//  NetworkManager.swift
+//  swiftui.lecture
+//
+//  Created by Aston Developer on 8/11/21.
+//
+
+import SwiftUI
+
+struct NetworkManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NetworkManager_Previews: PreviewProvider {
+    static var previews: some View {
+        NetworkManager()
+    }
+}
