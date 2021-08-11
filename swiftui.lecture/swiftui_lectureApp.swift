3be903f320bfa7e51bd11a5baae5494b8f903a1f
Bipin <developer.support@astontech.com>
2021-08-11 09:43:45 -0500

    Initial Commit

---
+++ swiftui.lecture/swiftui_lectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_lectureApp.swift
+//  swiftui.lecture
+//
+//  Created by Aston Developer on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_lectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
